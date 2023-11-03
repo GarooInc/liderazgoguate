0433940e289c7d256a2e638ab0673e3adaa34d84
Jimena Hernández <her21199@uvg.edu.gt>
2023-11-03 11:30:42 -0600

    update pics

--- speakers.js
+++ speakers.js
@@ -7,12 +7,12 @@ export const speakers  = [
     {
         name: 'Juan Sebastián Chamorro ',
         description: 'Economista y político nicaragüense. Director ejecutivo de la Fundación Nicaragüense para el Desarrollo Económico y Social (FUNIDES).',
-        picture: '/images/speakers/2.jpg',
+        picture: '/images/speakers/2.png',
     },
     {
         name: 'Representantes de Movimiento Cívico Nacional (MCN)',
         description: 'Movimiento Cívico Nacional (MCN) es una organización de la sociedad civil que busca promover la participación ciudadana, la democracia y la libertad en Nicaragua. ',
-        picture: '/images/speakers/3.png',
+        picture: '/images/speakers/6.png',
     },
     {
         name: 'Representantes de Guatemala Visible',
@@ -26,7 +26,7 @@ export const speakers  = [
     },{
         name: 'Representantes de Misión de Observación Electoral de Guatemala (MOE-Gt)',
         description: 'Misión de Observación Electoral de Guatemala (MOE-Gt) es una organización de la sociedad civil que busca promover la participación ciudadana, la democracia y la libertad en Nicaragua. ',
-        picture: '/images/speakers/6.png',
+        picture: '/images/speakers/3.png',
     }
 
 ]
--- src/components/Speaker/Speaker.jsx
+++ src/components/Speaker/Speaker.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 const Speaker = ({ speaker }) => {
     return (
         <div className="flex flex-col items-center justify-center p-4 rounded-2xl bg-mcn-blue h-96 shadow-2xl">
-            <img src={speaker.picture} alt={speaker.name} className="w-40 h-40 rounded-full object-fill shadow-2xl"/>
+            <img src={speaker.picture} alt={speaker.name} className="w-40 h-40 rounded-full object-fill shadow-2xl bg-white"/>
             <h3 className="text-xl font-bold text-center mt-8 text-white">{speaker.name}</h3>
         </div>
     )
